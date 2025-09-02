356aabf530e4c45bfb618f3f7bbf2cfbde48e1cc
Raman <kmitsolutionservices@gmail.com>
2025-09-02 09:28:48 +0530

    Add project files.

---
+++ gitproject-ascendion/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitproject_ascendion.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
